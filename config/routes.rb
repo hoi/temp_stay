499012e0b5ff30225e8958aeec9c69b3542404e2
Hyon Lee <hyonmail@gmail.com>
2015-09-17 11:25:45 -0700

    CSS moved off of default library

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get "/details" => "home#details"
-  get "/schedule" => "home#schedule"
+  get "/about" => "home#about"
   get "/faq" => "home#faq"
 
   root "home#index"
+  match "*unmatched_route" => redirect('/'), via: :all
 end
